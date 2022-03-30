ce43511e33e29c53b49f0e531b198c7eed6bf2d8
Ramiro Krapp <krappramiro@disroot.org>
2022-03-30 20:46:09 -0300

    adaptado main.cpp a nuevo enfoque

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 /*
-TODO Hacer el registro de viajes de cada pasajero
-
-*/
+ */
 
 #include <Arduino.h>
 #include <iostream>
@@ -15,112 +13,44 @@ https://www.thecrazyprogrammer.com/2021/01/better-alternatives-for-using-namespa
 */
 using std::vector;
 
-class Tren {
-	/* Si alguien se pregunta por qué las variables estan en private,
+/*
+	Si alguien se pregunta por qué, en las clases, las variables estan en private,
 	la respuesta es muy sencilla:
 	Es porque no se desea que se modifiquen las variables de forma manual.
 	Esto es porque esa práctica es propensa a errores, ya que se podría introducir
 	un valor inadecuado y generar algun problema.
 
 	Por eso se usan funciones public, normalmente llamadas setters, que permiten
-	asignar y leer los valores, y que establecen un margen de valores seguros.  */
-private:
-	int speed = 0; // velocidad, en km/h
-	String serialNumber; // numero de serie, que va a identificar al tren
-	String currentStation;
-	String trainType; // esta var se refiere si es a nafta, si es electrico, etc
-
-public:
-	Tren(String serialNumber, String trainType)
-	{
-		this->serialNumber = serialNumber;
-		this->trainType = trainType;
-	}
-	// Para los getters tenia dos opciones, o retornaba un struct, o hacia una funcion
-	// para cada variable
-	int getSpeed()
-	{
-		return speed;
-	}
-	String getSerialNumber()
-	{
-		return serialNumber;
-	}
-	String getCurrentStation()
-	{
-		return currentStation;
-	}
-	String getTrainType()
-	{
-		return trainType;
-	}
-
-	void travelToStation(String stationName)
-	{
-		currentStation = stationName;
-		// TODO Hacer algo parecido con la funcion que tenes en Pasajero
-	}
-};
-
-class Persona {
-	// Esta clase sirve como padre para las clases Maquinista y Pasajero
-	// IDEA: Hacer que las personas puedan morir, y que se invalide la SUBE.
-	// Por ejemplo,         if (!persona.isAlive) {allowTransaction(false)}
+	asignar y leer los valores, y que establecen un margen de valores seguros.
+*/
+class Empleado {
 private:
 	String name;
 	bool isAlive = true;
 	String dni;
+	int clearanceLevel;
+	String cargoAdministrativo;
 
 public:
-	void kill()
+	void setLifeStatus(bool lifeStatus)
 	{
-		isAlive = false;
+		this->isAlive = lifeStatus;
 	}
-};
-
-class Maquinista : public Persona { // clase que hereda de Persona
-private:
-	String name;
-	float salary;
-	int seniority; // el seniority se piensa con los años de antiguedad
-public:
-};
-
-class Pasajero : public Persona { // clase que hereda de Persona
-private:
-	String nombre;
-	int sube_id;
-	float sube_saldo;
-
-public:
-	void travelToStation(String stationName)
+	void setName(String name)
 	{
-		// TODO hay que hacer la transaccion
-
-		/*
-		Como deberia ser esto? tendria que ser así:
-		1- Calcular distancia a la estacion
-		2- Cobrar 5 pesos por cada estacion
-
-		Para calcular la estación, lo que haría sería armar un vector de
-		estaciones, algo asi:
-
-		["temperley", "lomas de zamora", "banfield", "remedios de escalada", "etc"]
-
-		1 - Llamar a una funcion getCurrentStation() que retorne un String
-		de la estacion actual
-		2 - sabiendo la estacion actual, se podría hacer un getIndex()
-		https://www.geeksforgeeks.org/how-to-find-index-of-a-given-element-in-a-vector-in-cpp/
-		Entonces se haria un getIndex(estacionActual) - getIndex(estacionDestino),
-		y el resultado de esa operacion es la distancia entre las estaciones.
-
-		OJO: Esa operacion puede dar resultados negativos, por eso habria que guardarlo
-		en una variable, checkear si es negativa, y en ese caso pasarla a positivo
-
-		3 - Llamar a la funcion calcularPasaje(int price_per_estation, int distance)
-
-
-		*/
+		this->name = name;
+	}
+	void setName(String dni)
+	{
+		this->dni = dni;
+	}
+	void setName(int clearanceLevel)
+	{
+		this->clearanceLevel = clearanceLevel;
+	}
+	void setName(String cargoAdministrativo)
+	{
+		this->cargoAdministrativo = cargoAdministrativo;
 	}
 };
 
