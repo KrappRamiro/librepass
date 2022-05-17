98089b074101f3ecc30ba36824e7bbb07b365f20
Ramiro Krapp <krappramiro@disroot.org>
2022-05-17 19:51:37 -0300

    Creados forms para envio de datos desde cliente a servidor
    Use unos forms en el html para mandar datos desde el cliente, y arme un
    codigo en el servidor para recibir cada uno de esos inputs, y que por
    ahora los Serial.printee

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,9 @@ te recomiendo personalmente este post,  es corto, sencillo, y bien explicado
 para principiantes:
 https://www.thecrazyprogrammer.com/2021/01/better-alternatives-for-using-namespace-std-in-c.html
 */
+const char* PARAM_INPUT_1 = "input1";
+const char* PARAM_INPUT_2 = "input2";
+const char* PARAM_INPUT_3 = "input3";
 class Empleado {
 	/*
 		Si alguien se pregunta por qué, en las clases, las variables estan en private,
@@ -123,9 +126,11 @@ int Empleado::cuentaEmpleados = 0;
 Empleado miEmpleado;
 
 //------------------ INICIO DE Configuracion de conexion a internet ----------------
-const char* ssid = "TeleCentro-882b"; // Nombre de la red
-const char* password = "ZGNJVMMHZ2MY"; // Contraseña de la red
+const char* ssid = "Krapp"; // Nombre de la red
+const char* password = "italodisco"; // Contraseña de la red
 AsyncWebServer server(80); // Inicio el web server en el puerto 80
+// Create an Event Source on /events
+AsyncEventSource events("/events");
 // ----------------- FIN DE Configuracion de conexion a internet ------------------
 
 // ----------------- INICIO DE Configuracion de servidor NTP -------------------
@@ -153,7 +158,8 @@ String getDateTime()
 	Returns:
 		String con la fecha y hora
 	*/
-	time_t now = time(nullptr);
+	time_t now
+		= time(nullptr);
 	struct tm* timeinfo = localtime(&now);
 	char buffer[80];
 	strftime(buffer, sizeof(buffer), "%d/%m/%Y %H:%M:%S", timeinfo);
@@ -322,7 +328,10 @@ int menu()
 	return (op - 48); // subtract 48 from read value, 48 is the zero from ascii table
 }
 
-const char index_html[] PROGMEM = R"rawliteral(
+const char index_html[] = R"rawliteral(
+
+
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -338,7 +347,7 @@ const char index_html[] PROGMEM = R"rawliteral(
 				font-family: Arial;
 				display: inline-block;
 				margin: 0px auto;
-				text-align: center;
+				text-align: left;
 			}
 			h2 {
 				font-size: 3rem;
@@ -354,6 +363,10 @@ const char index_html[] PROGMEM = R"rawliteral(
 				vertical-align: middle;
 				padding-bottom: 15px;
 			}
+			.user-input {
+				display: block;
+				margin: 10px;
+			}
 		</style>
 	</head>
 	<body>
@@ -361,70 +374,177 @@ const char index_html[] PROGMEM = R"rawliteral(
 		<p>
 			<i class="fas fa-thermometer-half" style="color: #059e8a"></i>
 			<span class="dht-labels">Temperature is: </span>
-			<span id="temperature">%TEMPERATURE%</span>
+			<span id="temperature-el">%TEMPERATURE%</span>
 			<sup class="units">&deg;C</sup>
 		</p>
 		<p>
 			<i class="fas fa-tint" style="color: #00add6"></i>
 			<span class="dht-labels">Humidity is: </span>
-			<span id="humidity">%HUMIDITY%</span>
+			<span id="humidity-el">%HUMIDITY%</span>
 			<sup class="units">&percnt;</sup>
 		</p>
 		<p>
 			<i class="fa-solid fa-clock"></i>
 			<span class="dht-labels">Date and hour is: </span>
-			<span id="date-hour">%DATE-HOUR%</span>
+			<span id="date-hour-el">%DATE-HOUR%</span>
 		</p>
+		<p>
+			<span class="dht-labels">Se leyo? :</span>
+			<span id="rfid-uid-el">%rfid-uid%</span>
+		</p>
+		<!-- User input -->
+		<!--
+		<label for="name-input-el">Nombre:</label>
+		<input
+			type="text"
+			name="name-input-el"
+			class="user-input"
+			id="name-input-el"
+		/>
+		<label for="dni-input-el"></label>
+		<input
+			type="text"
+			name="dni-input-el"
+			class="user-input"
+			id="dni-input-el"
+		/>
+		<label for="clearance-level-input-el"></label>
+		<input
+			type="text"
+			name="clearance-level-input-el"
+			class="user-input"
+			id="clearance-level-input-el"
+		/>
+		<label for="cargo-administrativo-input-el"></label>
+		<input
+			type="text"
+			name="cargo-administrativo-input-el"
+			class="user-input"
+			id="cargo-administrativo-input-el"
+		/>
+		<button id="save-input-btn">SAVE INPUT</button>
+		-->
+
+		<form action="/get">
+			input1: <input type="text" name="input1" />
+			<input type="submit" value="Submit" />
+		</form>
+		<br />
+		<form action="/get">
+			input2: <input type="text" name="input2" />
+			<input type="submit" value="Submit" />
+		</form>
+		<br />
+		<form action="/get">
+			input3: <input type="text" name="input3" />
+			<input type="submit" value="Submit" />
+		</form>
 	</body>
+
 	<script>
+		const saveInputBtn = document.getElementById("save-input-btn");
+
+		// -------------- INICIO DE Eventos -----------------
+		if (!!window.EventSource) {
+			//A partir de aca van todos los eventos del ESP32:
+			var source = new EventSource("/events");
+			source.addEventListener("open", function (e) {
+				console.log("Events Connected");
+			});
+			//Evento que se ejecuta cada vez que se recibe un error
+			source.addEventListener("error", function (e) {
+				if (e.target.readyState != EventSource.OPEN) {
+					console.log("Events Disconnected");
+				}
+			});
+			// Evento que se ejecuta cada vez que se recibe un mensaje cualquiera
+			source.addEventListener("message", function (e) {
+				console.log("message", e.data);
+			});
+			// Evento que se ejecuta cada vez que se recibe una lectura del rfid
+			source.addEventListener("rfidReadEvent", function (e) {
+				console.log("rfidReadEvent", e.data);
+				document.getElementById("rfid-uid-el").innerHTML = e.data;
+			});
+		}
+		// Creo que aca van los eventos de la propia pagina web
+		saveInputBtn.addEventListener("click", function () {
+			const person = {
+				name: document.getElementById("name-input-el").value,
+				dni: document.getElementById("dni-input-el").value,
+				clearanceLevel: document.getElementById("clearance-level-input-el")
+					.value,
+				cargoAdministrativo: document.getElementById(
+					"cargo-administrativo-input-el"
+				).value,
+			};
+			console.log("Read the following things:"); //log the saved lead
+			console.dir(person);
+		});
+
+		// ------------ FIN DE Eventos ------------
+
+		// ------------------ INICIO DE Intervalos ------------------
+		// Intervalo de temperatura
 		setInterval(function () {
 			var xhttp = new XMLHttpRequest();
 			xhttp.onreadystatechange = function () {
 				if (this.readyState == 4 && this.status == 200) {
-					document.getElementById("temperature").innerHTML = this.responseText;
+					document.getElementById("temperature-el").innerHTML =
+						this.responseText;
 				}
 			};
 			xhttp.open("GET", "/temperature", true); //esto llama al server.on() correspondiente
 			console.log("Updating temperature");
 			xhttp.send();
 		}, 3000);
+		// Intervalo de humedad
 		setInterval(function () {
 			var xhttp = new XMLHttpRequest();
 			xhttp.onreadystatechange = function () {
 				if (this.readyState == 4 && this.status == 200) {
-					document.getElementById("humidity").innerHTML = this.responseText;
+					document.getElementById("humidity-el").innerHTML = this.responseText;
 				}
 			};
 			xhttp.open("GET", "/humidity", true); //esto llama al server.on() correspondiente
 			console.log("updating humidity");
 			xhttp.send();
 		}, 3000);
+		// Intervalo de fecha y hora
 		setInterval(function () {
 			var xhttp = new XMLHttpRequest();
 			xhttp.onreadystatechange = function () {
 				if (this.readyState == 4 && this.status == 200) {
-					document.getElementById("date-hour").innerHTML = this.responseText;
+					document.getElementById("date-hour-el").innerHTML = this.responseText;
 				}
 			};
 			xhttp.open("GET", "/date-hour", true); //esto llama al server.on() correspondiente
 			console.log("updating date-hour");
 			xhttp.send();
 		}, 3000);
+		// ------------------ FIN DE Intervalos ------------------
 	</script>
 </html>
+
+
+
+
 )rawliteral";
+
+void notFound(AsyncWebServerRequest* request)
+{
+	request->send(404, "text/plain", "Not found");
+}
 
 String processor(const String& var)
 {
 	/*
-	Esta funcion procesa las variables que se encuentran en el index.html
+	Esta funcion procesa los placeholders que se encuentran en el index.html
 	Args:
-		var: String con la variable a procesar
+		var: String con el placeholder a procesar
 	Returns:
-		String con el valor de la variable procesada
+		String con el valor de el placeholder procesado
 	*/
-	// Estos consiguiendo x... estan para testear nomas, en realidad no consiguen nada,
-	// estan para que no se me buggee el cerebro mientras entiendo AJAX
 	Serial.println(var);
 	if (var == "TEMPERATURE") {
 		return String("Consiguiendo temperatura...");
@@ -432,6 +552,8 @@ String processor(const String& var)
 		return String("Consiguiendo humedad...");
 	} else if (var == "HOUR") {
 		return String("Consiguiendo hora...");
+	} else if (var == "SE_LEYO") {
+		return String("Esperando lectura");
 	} else {
 		return String("");
 	}
@@ -459,6 +581,7 @@ void setup()
 	Serial.println(WiFi.localIP());
 
 	// --------------- INICIO DE Rutas del servidor -----------------
+	// ----- GET's
 	// Ruta para el index.html
 	server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
 		request->send_P(200, "text/html", index_html, processor);
@@ -475,9 +598,47 @@ void setup()
 	server.on("/date-hour", HTTP_GET, [](AsyncWebServerRequest* request) {
 		request->send_P(200, "text/plain", String(getDateTime()).c_str());
 	});
-	// ---------------- FIN DE Rutas del servidor -------------------
-	server.begin();
 
+	// Send a GET request to <ESP_IP>/get?input=X<inputMessage>
+	server.on("/get", HTTP_GET, [](AsyncWebServerRequest* request) {
+		String inputMessage;
+		String inputParam;
+		// GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+		if (request->hasParam(PARAM_INPUT_1)) {
+			inputMessage = request->getParam(PARAM_INPUT_1)->value();
+			inputParam = PARAM_INPUT_1;
+		}
+		// GET input2 value on <ESP_IP>/get?input2=<inputMessage>
+		else if (request->hasParam(PARAM_INPUT_2)) {
+			inputMessage = request->getParam(PARAM_INPUT_2)->value();
+			inputParam = PARAM_INPUT_2;
+		}
+		// GET input3 value on <ESP_IP>/get?input3=<inputMessage>
+		else if (request->hasParam(PARAM_INPUT_3)) {
+			inputMessage = request->getParam(PARAM_INPUT_3)->value();
+			inputParam = PARAM_INPUT_3;
+		} else {
+			inputMessage = "No message sent";
+			inputParam = "none";
+		}
+		Serial.println(inputMessage);
+		request->send(204);
+	});
+
+	// ----- Eventos
+	events.onConnect([](AsyncEventSourceClient* client) {
+		if (client->lastId()) {
+			Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+		}
+		// send event with message "hello!", id current millis
+		// and set reconnect delay to 1 second
+		client->send("hello!", NULL, millis(), 10000);
+	});
+
+	server.addHandler(&events);
+	// ---------------- FIN DE Rutas del servidor -------------------
+
+	server.begin();
 	// Cosas del ntp server para la fecha/hora
 	configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 	// Inicio el MFRC552
@@ -488,6 +649,7 @@ void setup()
 
 void loop()
 {
+	// ------- INICIO DEL LECTOR RFID --------------
 	// Se espera a que se acerque un tag
 	if (!mfrc522.PICC_IsNewCardPresent()) {
 		return;
@@ -496,10 +658,12 @@ void loop()
 	if (!mfrc522.PICC_ReadCardSerial()) {
 		return;
 	}
-	// Descomentar solamente si se quiere Dumpear toda la info acerca de la tarjeta leida
-	// Ojo que llama automaticamente a PICC_HaltA()
+	// Descomentar solamente si se quiere Dumpear toda la info acerca de la tarjeta leida, ojo que llama automaticamente a PICC_HaltA()
 	// mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 
+	// Le digo al servidor que se leyo el tag RFID
+	events.send(getUID().c_str(), "rfidReadEvent");
+	/*
 	// LLama a la funcion de menu para que el usuario elija una opcion
 	int op = menu();
 	if (op == 0)
@@ -518,6 +682,7 @@ void loop()
 		Serial.println(F("Incorrect Option!"));
 		return;
 	}
+	*/
 
 	// Le dice al PICC que se vaya a un estado de STOP cuando esta activo (o sea, lo haltea)
 	mfrc522.PICC_HaltA();
@@ -526,4 +691,5 @@ void loop()
 	// Tiene que ser llamado si o si despues de la comunicacion con una
 	// autenticación exitosa, en otro caso no se va a poder iniciar otra comunicación.
 	mfrc522.PCD_StopCrypto1();
+	// ------- FIN DEL LECTOR RFID --------------
 }
